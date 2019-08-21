9dc5b794edae959acb8d50fab10b91470da59bb8
stephenjude <stephenjudesuccess@gmail.com>
2019-08-21 13:12:42 +0100

    Fix testcase not found

--- tests/PaystackBladeDirectiveTest.php
+++ tests/PaystackBladeDirectiveTest.php
@@ -2,15 +2,20 @@
 
 namespace Stephenjude\PaystackLite\Tests;
 
+use PHPUnit\Framework\TestCase;
 
-
-class PaystackBladeDirectiveTest extends Orchestra\Testbench\TestCase
+class PaystackBladeDirectiveTest extends TestCase
 {
     private $blade;
 
     public function setUp(): void
     {
         parent::setUp();
+    }
+
+    public function test_example()
+    {
+        $this->assertTrue(true);
     }
 
 }
